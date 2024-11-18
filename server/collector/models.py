59d2ae81fc6d7bebdc4cce290e2d0cf084e16c41
gladyswhm <gladyswhm@gmail.com>
2024-11-18 12:47:43 +0800

    upload collector in server files

---
+++ server/collector/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Collector(models.Model):
+    objects = None
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15, blank=True)
+    fax = models.CharField(max_length=15, blank=True)
+    licences = models.CharField(max_length=23)
+    lat = models.FloatField(null=True, blank=True)  
+    lng = models.FloatField(null=True, blank=True) 
+    suspended = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
---
+++ server/collector/urls.py
@@ -0,0 +1,12 @@
+# urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CollectorViewSet
+
+router = DefaultRouter()
+router.register(r'collectors', CollectorViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/', include(router.urls))
+]
